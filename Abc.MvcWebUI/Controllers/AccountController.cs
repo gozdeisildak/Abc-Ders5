a6ab83d4d18fb06101d86bee3e26cf07ca678755
gozdeisildak <gozdeisildak@gmail.com>
2020-05-17 15:58:18 +0300

    Add project files.

---
+++ Abc.MvcWebUI/Controllers/AccountController.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using Abc.MvcWebUI.Identity;
+using Abc.MvcWebUI.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin.Security;
+
+namespace Abc.MvcWebUI.Controllers
+{
+    public class AccountController : Controller
+    {
+        private UserManager<ApplicationUser> UserManager;
+        private RoleManager<ApplicationRole> RoleManager;
+
+        public AccountController()
+        {
+            var userStore = new UserStore<ApplicationUser>(new IdentityDataContext());
+            UserManager = new UserManager<ApplicationUser>(userStore);
+
+            var roleStore = new RoleStore<ApplicationRole>(new IdentityDataContext());
+            RoleManager = new RoleManager<ApplicationRole>(roleStore);
+
+        }
+
+        // GET: Account
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(Register model)
+        {
+            if (ModelState.IsValid)
+            {
+                //Kayıt işlemleri
+
+                var user = new ApplicationUser();
+                user.Name = model.Name;
+                user.Surname = model.SurName;
+                user.Email = model.Email;
+                user.UserName = model.UserName;
+
+                var result = UserManager.Create(user, model.Password);
+
+                if (result.Succeeded)
+                {
+                    //kullanıcı oluştu ve kullanıcıyı bir role atayabilirsiniz.
+                    if (RoleManager.RoleExists("user"))
+                    {
+                        UserManager.AddToRole(user.Id, "user");
+                    }
+                   
+                    return RedirectToAction("Login", "Account");
+                }
+                else
+                {
+                    ModelState.AddModelError("RegisterUserError", "Kullanıcı  oluşturma hatası.");
+                }
+
+            }
+
+            return View(model);
+        }
+
+        // GET: Account
+        public ActionResult Login()
+        {
+            if (String.IsNullOrEmpty(HttpContext.User.Identity.Name))
+            {
+                FormsAuthentication.SignOut();
+                return View();
+            }
+            return Redirect("/Home/Index");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Login(Login model,string ReturnUrl)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    //Login işlemleri
+                    var user = await UserManager.FindAsync(model.UserName, model.Password);
+                    var role = RoleManager.FindByName("admin");
+                    if (user != null)
+                    {
+                        // varolan kullanıcıyı sisteme dahil et.
+                        // ApplicationCookie oluşturup sisteme bırak.
+
+                        var authManager = HttpContext.GetOwinContext().Authentication;
+                        var identityclaims =await UserManager.CreateIdentityAsync(user, DefaultAuthenticationTypes.ApplicationCookie);
+                        var authProperties = new AuthenticationProperties();
+                        authProperties.IsPersistent = false;
+                        authManager.SignIn(authProperties, identityclaims);
+
+                       
+                        if (user.Roles.Select(i => i.RoleId).First() == role.Id)
+                        {
+                            return RedirectToAction("Index", "Product");
+                        }
+                        else
+                        {
+                            return RedirectToAction("Index", "Home");
+                        }
+
+
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("LoginUserError", "Böyle bir kullanıcı yok.");
+                    }
+                }
+            }
+            catch (Exception)
+            {
+
+                ModelState.AddModelError("LoginUserError", "Böyle bir kullanıcı yok.");
+            }
+           
+
+            return View(model);
+        }
+
+        public ActionResult Logout()
+        {
+            var authManager = HttpContext.GetOwinContext().Authentication;
+            authManager.SignOut();
+
+            return RedirectToAction("Index","Home");
+        }
+
+    }
+}
---
+++ Abc.MvcWebUI/Identity/MembershipTools.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Abc.MvcWebUI.Identity
+{
+    public class MembershipTools
+    {
+        //private MyContext DataContext;
+
+    }
+}
