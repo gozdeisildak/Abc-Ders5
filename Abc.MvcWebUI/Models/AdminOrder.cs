25587998bd211e5a085577b0a4197117f23ff663
bahar-yilmaz20 <bahar.yilmaz@ceng.deu.edu.tr>
2020-06-02 06:41:47 +0300

    Revert "Revert "Validations""
    This reverts commit 265c26f5a46f205b221af80214efdd3eecb57721.

---
+++ Abc.MvcWebUI/Models/AdminOrder.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Abc.MvcWebUI.Models
+{
+    public class AdminOrder
+    {
+    }
+}
--- Abc.MvcWebUI/Models/Cart.cs
+++ Abc.MvcWebUI/Models/Cart.cs
@@ -47,6 +47,7 @@ namespace Abc.MvcWebUI.Models
     public class CartLine
     {
         public Product Product { get; set; }
-        public int Quantity { get; set; }       
+        public int Quantity { get; set; }
+       
     }
 }
